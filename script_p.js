91aba0ce853526c8ea1707b36fd1305bc5d69757
Your Name <you@example.com>
2022-03-04 10:48:27 +0100

    finalisation du projet

--- script_p.js
+++ script_p.js
@@ -1,19 +1,20 @@
 function previous(genre) {
     let movies_selector = $(`#${genre}`)
     movies_selector.append(movies_selector.children()[0])
-    update()
+    update(genre)
    }
+
 
 function next(genre) {
     let movies_selector = $(`#${genre}`)
     movies_selector.prepend(movies_selector.children()[6])
-    update()
+    update(genre)
    }
 
 function update(genre) {
     let movies_selector = $(`#${genre}`)
     let children = movies_selector.children()
-    children.each((index, elem)=> {
+    children.each((index, elem)=> { 
         if( index < 4 ) {
             $(elem).show()
         } else {
@@ -22,84 +23,107 @@ function update(genre) {
     })
    }
 
-//function qui va ajouter les information dans le modal une par div dans les modal
-async function modal_try_un(id){
-    const response = await fetch(`http://127.0.0.1:8000/api/v1/titles/${id}`)
-    const data = await response.json()
-    movie_detail = data
-    for(const [key, value] of Object.entries(data)) {
-        let modal_info = document.getElementById(key)
-        if (modal_info != undefined){
-            document.querySelector(`#modal > #${key} > .title`).innerHMTL = key
-            document.querySelector(`#modal > #${value} > .title`).innerHTML = value
-        }
-        // //on les ajoute une par une dans les div grâce a la boucle for
-        // div = movie_detail[description_une_par_une]
-        // modal.innerHTML = div
-    }
-   }
-
-
-function modal_un(image_url){
-    modal = document.getElementById("modal")
-    img = document.createElement("img")
-    modal.appendChild(img)
-    img.src = image_url.src
+function all_name(){
+    carousel_cate("romance", "blue")
+    carousel_cate("action", "green")
+    carousel_cate("drama", "white")
 }
 
-function openWin(url){
+//function qui va ajouter les information dans le modal une par div dans les modal
+
+function carousel_cate(genre, color= string){
+    let test = document.getElementById(genre+"_carousel")
+    test.style.width = "1000px"
+    test.style.heigh = "200px"
+    test.style.fontSize = "32px"
+    test.style.margin = "auto"
+    test.style.position = "relative"
+    test.style.display = "fixed"
+    test.style.padding = "10px"
+    test.style.borderBottom = "solid"
+    test.style.color = color
+}
+
+
+async function setup_modal(movie_id){
+    const response = await fetch(`http://127.0.0.1:8000/api/v1/titles/${movie_id}`)
+    const data = await response.json()
+    document.getElementById("modal").style.display= "block";
+    movie_detail = data
+    for(const [key, value] of Object.entries(data)) {
+        let info = document.getElementById(key)
+        if (info != undefined) {
+          info.innerHTML = `<div class="title">${key} : ${value}</div>`
+        if(info == duration){
+            info.innerHTML =`<div class="title">${key} : ${value} minute</div>`
+        }
+          if (info == image_url){
+            info.innerHTML = `<img src="${value}"></img>`
+          }
+        }        
+      }
+    }
+
+
+function open_modal(url){
     newwin=window.open(url,'','width=400,height=400,top=200,left=200')
 }
 
-function chooce_carousel(genre){
+function close_modal() {
+    document.getElementById("modal").style.display= "none";
+}
+
+function choose_carousel(genre){
     let id = document.getElementById(genre)
     return id
 }
 
-
-let get_data = async(genre)=>{
-    const response = await fetch(`http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=${genre}`)
-    const data = await response.json()
-    return data.results
-}
-
-let get_url = async()=>{
-    const test = document.getElementById("test_click").textContent
-    const response = await fetch(test)
-    const data = await response.json()
-    return data
-}
-
-let get_best_movie = async()=>{
-    const response = await fetch("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&page_size=1")
-    const data = await response.json()
-    let div = document.getElementById("best_movie")
-    let lien = document.createElement('a')
-    lien.title = data.results[0].title
-    lien.setAttribute("onClick",openWin(data.results[0].imdb_url))
-    let img = document.createElement("img")
-    img.src = data.results[0].image_url
-    modal_un(img)
-    div.appendChild(lien)
-    lien.appendChild(img)
-  }
-
-//   <a href="${movie_data.imdb_url}">
-
-
-let get_data_movies = async(genre)=>{
-    const movies = await get_data(genre)
+let get_data_carousel_movies = async () => {
+    const movies = await get_best_data()
     movies.forEach((movie_data, index) => {
-        $(`#movie_${index+1}_${genre}`).html(`
+        $(`#movie_${index+1}_best`).html(`
             <img src="${movie_data.image_url}" onclick="modal_try_un(${movie_data.id})">
         `)
     });
 }
 
+let get_movies = async (genre = "") => {
+    const response = await fetch(`http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score&sort_by=-votes&page_size=7&genre=${genre}`)
+    const data = await response.json()
+    return data.results
+}
+
+let setup_best_movie = async () => {
+    const movie_data = await get_movies()
+    $(`#best_movie`).html(`
+        <img src="${movie_data[0].image_url}" onclick="setup_modal(${movie_data[0].id})"></img>
+    `) 
+  }
+
+let setup_carousel = async (genre = "") => {
+    const movies = await get_movies(genre)
+    movies.forEach((movie_data, index) => {
+        if (genre === ""){
+            genre = "best"
+        }
+        $(`#movie_${index+1}_${genre}`).html(`
+            <img src="${movie_data.image_url}" onclick="setup_modal(${movie_data.id})">
+        `)
+    });
+}
+
+function all_carousel(){
+    setup_carousel("romance")
+    setup_carousel("drama")
+    setup_carousel("action")
+    setup_best_movie()
+    setup_carousel()
+
+}
+
+
 document.addEventListener('DOMContentLoaded',function() {
-    get_data_movies("romance")
-    get_data_movies("drama")
-    get_data_movies("action")
-    get_best_movie()
+    all_carousel()
+    all_name()
 })
 
